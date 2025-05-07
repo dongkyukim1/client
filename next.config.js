267e291afdd5bf51e9d162365fd669ab04f73349
양정운 <yp071704@gmail.com>
2025-05-07 20:19:10 +0900

    chore: http proxy

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require("path");
 
 const nextConfig = {
-  transpilePackages: ['@chakra-ui/react', '@chakra-ui/next-js'],
+  transpilePackages: ["@chakra-ui/react", "@chakra-ui/next-js"],
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'randomuser.me' },
-      { protocol: 'http', hostname: 'tong.visitkorea.or.kr' },
-      { protocol: 'https', hostname: 'tong.visitkorea.or.kr' },
-      { protocol: 'https', hostname: 'api.visitkorea.or.kr' },
-      { protocol: 'https', hostname: 'korean.visitkorea.or.kr' },
-      { protocol: 'https', hostname: 'www.visitkorea.or.kr' },
-      { protocol: 'https', hostname: 'cdn.pixabay.com' }
+      { protocol: "https", hostname: "randomuser.me" },
+      { protocol: "http", hostname: "tong.visitkorea.or.kr" },
+      { protocol: "https", hostname: "tong.visitkorea.or.kr" },
+      { protocol: "https", hostname: "api.visitkorea.or.kr" },
+      { protocol: "https", hostname: "korean.visitkorea.or.kr" },
+      { protocol: "https", hostname: "www.visitkorea.or.kr" },
+      { protocol: "https", hostname: "cdn.pixabay.com" },
     ],
-    domains: ['cdn.pixabay.com', 'apis.data.go.kr', 'tong.visitkorea.or.kr', '43.200.177.95'],
-    unoptimized: true
+    domains: ["cdn.pixabay.com", "apis.data.go.kr", "tong.visitkorea.or.kr", "43.200.177.95"],
+    unoptimized: true,
   },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     };
     config.resolve.fallback = {
       ...config.resolve.fallback,
@@ -32,11 +32,11 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://3.39.0.103/api/:path*', // 실제 API 서버
+        source: ":path*",
+        destination: "http://3.34.181.101/:path*",
       },
     ];
   },
-}
+};
 
 module.exports = nextConfig;
